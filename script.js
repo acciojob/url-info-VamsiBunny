6a3b3b52867e804d233a0173124566095cb5ece6
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-07 20:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function url(str){
+
+	let count=0;
+  for(let i=0;i<str.length;i++){
+
+	  count++
+  
+  }
+	return count;
+	
+}
+const str = prompt("Enter url");
+alert(url(str));
