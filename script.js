a2343dce66970c0370c2fd04d011529a68066c15
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-07 21:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function url(str) {
-  if (!str) {  
-    return 0;  
-  }
 
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    count++;
-  }
-  return count;
-}
+let currentURL = window.location.href;
 
-const str = prompt("Enter url");
-alert("The length of the URL is: " + url(str));
+
+let urlLength = currentURL.length;
+
+alert("The length of the URL is: " + urlLength);
