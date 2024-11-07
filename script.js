729de211164ec12b4d5576bb02328bf44d298c89
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-07 21:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-//your JS code here. If required.
-function url(str){
-
-	 if (str === null) {
+function url(str) {
+  if (!str) {  
     return 0;  
   }
-	let count=0;
-  for(let i=0;i<str.length;i++){
 
-	  count++
-  
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    count++;
   }
-	return count;
-	
+  return count;
 }
-const str = prompt("Enter url"  );
-alert(url(str));
+
+const str = prompt("Enter url");
+alert("The length of the URL is: " + url(str));
